cc3bbd1b8bc874684ada51390c5bd62ae08b44b7
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2023-03-06 08:36:52 -0500

    Use recur only within loop
    From initial analysis, so far I've only used recur to target a
    function, rather than loop, for set/intersection

--- lib/src/pilisp_core_pilisp.dart
+++ lib/src/pilisp_core_pilisp.dart
@@ -1096,13 +1096,14 @@ final corePiLisp = r'''
   {:doc "Return a set that is the intersection of the input sets"}
   ([s1] s1)
   ([s1 s2]
-   (if (< (count s2) (count s1))
-     (recur s2 s1)
-     (reduce (fn [result item]
-               (if (contains? s2 item)
-		 result
-                 (disj result item)))
-	     s1 s1)))
+   (loop [s1 s1 s2 s2]
+     (if (< (count s2) (count s1))
+       (recur s2 s1)
+       (reduce (fn [result item]
+                 (if (contains? s2 item)
+                   result
+                   (disj result item)))
+               s1 s1))))
   ([s1 s2 & sets]
    (let [bubbled-sets (bubble-max-key #(- (count %)) (conj sets s2 s1))]
      (reduce set/intersection (first bubbled-sets) (rest bubbled-sets)))))
